c10e297d1c6e34805c039dfa00ae946e4d1281f1
Muhammad Ahmad <ahmadcs442@gmail.com>
2023-05-27 22:51:44 +0500

    all models trained

--- app.py
+++ app.py
@@ -8,16 +8,61 @@ from sklearn.feature_extraction.text import CountVectorizer
 app = Flask(__name__)
 
 # Load the trained machine learning model
-with open('MultinomialNB_KFold.pkl', 'rb') as f:
+# Loading the MultinomialNB model
+with open('./models/MultinomialNB_KFold.pkl', 'rb') as f:
     MultinomialNB_KFold = pickle.load(f)
+with open('./models/MultinomialNB_Train_Test.pkl', 'rb') as f:
+    MultinomialNB_Train_Test = pickle.load(f)
+# Loading the AdaBoost Classifier model
+with open('./models/AdaBoost_Classifier_KFold.pkl', 'rb') as f:
+    AdaBoost_Classifier_KFold = pickle.load(f)
+with open('./models/AdaBoost_Classifier_Train_Test.pkl', 'rb') as f:
+    AdaBoost_Classifier_Train_Test = pickle.load(f)
+# Loading the Bagging Classifier model
+with open('./models/Bagging_Classifier_KFold.pkl', 'rb') as f:
+    Bagging_Classifier_KFold = pickle.load(f)
+with open('./models/Bagging_Classifier_Train_Test.pkl', 'rb') as f:
+    Bagging_Classifier_Train_Test = pickle.load(f)
+# Loading the Decision Tree model
+with open('./models/Decision_Tree_KFold.pkl', 'rb') as f:
+    Decision_Tree_KFold = pickle.load(f)
+with open('./models/Decision_Tree_Train_Test.pkl', 'rb') as f:
+    Decision_Tree_Train_Test = pickle.load(f)
+# Loading the GaussianNB model
+with open('./models/GaussianNB_KFold.pkl', 'rb') as f:
+    GaussianNB_KFold = pickle.load(f)
+with open('./models/GaussianNB_Train_Test.pkl', 'rb') as f:
+    GaussianNB_Train_Test = pickle.load(f)
+# Loading the Gradient Boosting Classifier model
+with open('./models/Gradient_Boosting_Classifier.pkl', 'rb') as f:
+    Gradient_Boosting_Classifier_KFold = pickle.load(f)
+with open('./models/Gradient_Boosting_Classifier_Train_Test.pkl', 'rb') as f:
+    Gradient_Boosting_Classifier_Train_Test = pickle.load(f)
+# Loading the KNN model
+with open('./models/KNN_KFold.pkl', 'rb') as f:
+    KNN_KFold = pickle.load(f)
+with open('./models/KNN_Train_Test.pkl', 'rb') as f:
+    KNN_Train_Test = pickle.load(f)
+# Loading the Logistic Regression model
+with open('./models/Logistic_Regression_KFold.pkl', 'rb') as f:
+    Logistic_Regression_KFold = pickle.load(f)
+with open('./models/Logistic_Regression_Train_Test.pkl', 'rb') as f:
+    Logistic_Regression_Train_Test = pickle.load(f)
+# Loading the Random Forest model
+with open('./models/Random_Forest_KFold.pkl', 'rb') as f:
+    Random_Forest_KFold = pickle.load(f)
+with open('./models/Random_Forest_Train_Test.pkl', 'rb') as f:
+    Random_Forest_Train_Test = pickle.load(f)
+# Loading the SVC model
+with open('./models/SVC_KFold.pkl', 'rb') as f:
+    SVC_KFold = pickle.load(f)
+with open('./models/SVC_Train_Test.pkl', 'rb') as f:
+    SVC_Train_Test = pickle.load(f)
 
-# Load the vectorizer for train test split
-with open('vectorizer_train_test.pkl', 'rb') as f:
+
+# Load the vectorizer
+with open('vectorizer.pkl', 'rb') as f:
     vectorizer = pickle.load(f)
-
-# Load the vectorizer for KFold
-with open('vectorizer_KFold.pkl', 'rb') as f:
-    vectorizer_KFold = pickle.load(f)
 
 
 @app.route('/')
@@ -33,39 +78,80 @@ def prediction():
         split = request.form.get('split')
         model1 = request.form.get('model')
 
-        # Perform prediction
-        email_vectorized_KFold = vectorizer_KFold.transform([email])
+        # Vectorize the text using the loaded vectorizer
         email_vectorized = vectorizer.transform([email])
         predicted_label = None
+
+        # Make a prediction using the loaded model
         if(split == 'KFold' and model1 == 'MultinomialNB'):
+            print(split, model1)
             predicted_label = MultinomialNB_KFold.predict(
-                email_vectorized_KFold)
+                email_vectorized)
+        elif(split == 'Train_Test' and model1 == 'MultinomialNB'):
+            print(split, model1)
+            predicted_label = MultinomialNB_Train_Test.predict(
+                email_vectorized)
+        elif(split == 'KFold' and model1 == 'AdaBoost_Classifier'):
+            predicted_label = AdaBoost_Classifier_KFold.predict(
+                email_vectorized)
+        elif(split == 'Train_Test' and model1 == 'AdaBoost_Classifier'):
+            predicted_label = AdaBoost_Classifier_Train_Test.predict(
+                email_vectorized)
+        elif(split == 'KFold' and model1 == 'Bagging_Classifier'):
+            predicted_label = Bagging_Classifier_KFold.predict(
+                email_vectorized)
+        elif(split == 'Train_Test' and model1 == 'Bagging_Classifier'):
+            predicted_label = Bagging_Classifier_Train_Test.predict(
+                email_vectorized)
+        elif(split == 'KFold' and model1 == 'Decision_Tree'):
+            predicted_label = Decision_Tree_KFold.predict(
+                email_vectorized)
+        elif(split == 'Train_Test' and model1 == 'Decision_Tree'):
+            predicted_label = Decision_Tree_Train_Test.predict(
+                email_vectorized)
+        elif(split == 'KFold' and model1 == 'GaussianNB'):
+            predicted_label = GaussianNB_KFold.predict(
+                email_vectorized)
+        elif(split == 'Train_Test' and model1 == 'GaussianNB'):
+            predicted_label = GaussianNB_Train_Test.predict(
+                email_vectorized)
+        elif(split == 'KFold' and model1 == 'Gradient_Boosting_Classifier'):
+            predicted_label = Gradient_Boosting_Classifier_KFold.predict(
+                email_vectorized)
+        elif(split == 'Train_Test' and model1 == 'Gradient_Boosting_Classifier'):
+            predicted_label = Gradient_Boosting_Classifier_Train_Test.predict(
+                email_vectorized)
+        elif(split == 'KFold' and model1 == 'KNN'):
+            predicted_label = KNN_KFold.predict(
+                email_vectorized)
+        elif(split == 'Train_Test' and model1 == 'KNN'):
+            predicted_label = KNN_Train_Test.predict(
+                email_vectorized)
+        elif(split == 'KFold' and model1 == 'Logistic_Regression'):
+            predicted_label = Logistic_Regression_KFold.predict(
+                email_vectorized)
+        elif(split == 'Train_Test' and model1 == 'Logistic_Regression'):
+            predicted_label = Logistic_Regression_Train_Test.predict(
+                email_vectorized)
+        elif(split == 'KFold' and model1 == 'Random_Forest'):
+            predicted_label = Random_Forest_KFold.predict(
+                email_vectorized)
+        elif(split == 'Train_Test' and model1 == 'Random_Forest'):
+            predicted_label = Random_Forest_Train_Test.predict(
+                email_vectorized)
+        elif(split == 'KFold' and model1 == 'SVC'):
+            predicted_label = SVC_KFold.predict(
+                email_vectorized)
+        elif(split == 'Train_Test' and model1 == 'SVC'):
+            predicted_label = SVC_Train_Test.predict(
+                email_vectorized)
+        else:
+            predicted_label = ['Invalid Input']
+
         print(predicted_label[0])
-        # Map predicted label to human-readable text
-        # if predicted_label[0] == 0:
-        #     prediction_text = 'Not Spam'
-        # else:
-        #     prediction_text = 'Spam'
-        # Return the result to the template
-        return render_template('prediction.html', email=email, prediction=predicted_label[0])
+        return render_template('prediction.html', email=email, prediction=predicted_label[0],split = split, model = model1)
     else:
         return render_template('prediction.html')
-
-# @app.route('/prediction', methods=['POST'])
-# def predict():
-#     # Get the input values from the HTML form
-#     features = [float(x) for x in request.form.values()]
-
-#     # Make the prediction using the loaded model
-#     prediction = model.predict([features])
-
-#     # Perform any additional processing with the prediction (e.g., convert to text label)
-#     if prediction[0] == 0:
-#         result = 'Not Spam'
-#     else:
-#         result = 'Spam'
-
-#     return render_template('result.html', prediction=result)
 
 
 if __name__ == '__main__':
